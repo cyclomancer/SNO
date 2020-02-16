9ce7c025e3cb692b6d37eb3cabec7b8e554d5339
Armitage <ygyzyx@gmail.com>
2020-02-15 19:40:36 -0500

    IT'S ALIVE

--- ScenarioBondingCurve.sol
+++ ScenarioBondingCurve.sol
@@ -1,93 +1,157 @@
 pragma solidity ^0.6.1;
 
-import "./SafeMath.sol";
+contract DSMath {
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x, "ds-math-add-overflow");
+    }
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x, "ds-math-sub-underflow");
+    }
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, "ds-math-mul-overflow");
+    }
 
-contract ScenarioBondingCurve {
-    using SafeMath for uint256;
+    function min(uint x, uint y) internal pure returns (uint z) {
+        return x <= y ? x : y;
+    }
+    function max(uint x, uint y) internal pure returns (uint z) {
+        return x >= y ? x : y;
+    }
+    function imin(int x, int y) internal pure returns (int z) {
+        return x <= y ? x : y;
+    }
+    function imax(int x, int y) internal pure returns (int z) {
+        return x >= y ? x : y;
+    }
 
+    uint constant WAD = 10 ** 18;
+    uint constant RAY = 10 ** 27;
+
+    function wmul(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, y), WAD / 2) / WAD;
+    }
+    function rmul(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, y), RAY / 2) / RAY;
+    }
+    function wdiv(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, WAD), y / 2) / y;
+    }
+    function rdiv(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, RAY), y / 2) / y;
+    }
+
+    // This famous algorithm is called "exponentiation by squaring"
+    // and calculates x^n with x as fixed-point and n as regular unsigned.
+    //
+    // It's O(log n), instead of O(n) for naive repeated multiplication.
+    //
+    // These facts are why it works:
+    //
+    //  If n is even, then x^n = (x^2)^(n/2).
+    //  If n is odd,  then x^n = x * x^(n-1),
+    //   and applying the equation for even x gives
+    //    x^n = x * (x^2)^((n-1) / 2).
+    //
+    //  Also, EVM division is flooring and
+    //    floor[(n-1) / 2] = floor[n / 2].
+    //
+    function rpow(uint x, uint n) internal pure returns (uint z) {
+        z = n % 2 != 0 ? x : RAY;
+
+        for (n /= 2; n != 0; n /= 2) {
+            x = rmul(x, x);
+
+            if (n % 2 != 0) {
+                z = rmul(z, x);
+            }
+        }
+    }
+}
+
+contract ScenarioBondingCurve is DSMath {
+    
     address payable public beneficiary;
-    uint256 public currentSupply;
-    uint256 public totalContributed;
-    mapping (address => uint256) public ledger;
-    mapping (address => uint256) public contributions;
+    uint public currentSupply;
+    uint public totalContributed;
+    mapping (address => uint) public ledger;
+    mapping (address => uint) public contributions;
 
-    uint8 public exponent;
-    uint256 public coefficient;
-    uint256 public reserveRatio;
-
-    uint256 private constant precision = 1000000000000;
+    uint public exponent;
+    uint public coefficient;
+    uint public reserveRatio;
+    
+    uint public precision = 1000000000000000000;
 
     string internal constant INSUFFICIENT_ETH = 'Insufficient Ether';
     string internal constant INSUFFICIENT_TOKENS = 'Request exceeds token balance';
+    string internal constant INVALID_ADDRESS = 'Wallet does not exist';
 
     constructor()
     public {
-        beneficiary = 0x225991BbF363a9ffE3aD0ebb9d6cFe7e79Cdb3FF;
+        beneficiary = 0x4aB6A3307AEfcC05b9de8Dbf3B0a6DEcEBa320E6;
         exponent = 2;
-        coefficient = 1000000000;
-        reserveRatio = 800000000000;
+        coefficient = 10000000000000;
+        reserveRatio = wdiv(4, 5);
         currentSupply = 1;
     }
-
-    function buy(uint256 amount)
+    
+    function buy(uint amount)
     external payable {
-        uint256 price = calcMintPrice(amount);
+        uint price = calcMintPrice(amount);
         require(msg.value >= price, INSUFFICIENT_ETH);
-        uint256 reserveValue = msg.value.mul(reserveRatio).div(precision);
-        uint256 contributionValue = msg.value.sub(reserveValue);
-        uint256 refund = msg.value.sub(reserveValue).sub(contributionValue);
+        uint reserveValue = wmul(msg.value, reserveRatio);
+        uint contributionValue = sub(msg.value, reserveValue);
+        uint refund = msg.value - reserveValue - contributionValue;
         if (refund > 0) {
             msg.sender.transfer(refund);
         }
-        ledger[msg.sender] = ledger[msg.sender].add(amount);
-        currentSupply = currentSupply.add(amount);
+        ledger[msg.sender] = add(ledger[msg.sender], amount);
+        currentSupply = add(currentSupply, amount);
         contribute(contributionValue, msg.sender);
     }
 
-    function sell(uint256 amount)
+    function sell(uint amount)
     external {
         require(amount <= ledger[msg.sender], INSUFFICIENT_TOKENS);
-        uint256 exitValue = calcBurnReward(amount);
+        uint exitValue = calcBurnReward(amount);
         msg.sender.transfer(exitValue);
-        ledger[msg.sender] = ledger[msg.sender].sub(amount);
-        currentSupply = currentSupply.sub(amount);
+        ledger[msg.sender] = sub(ledger[msg.sender], amount);
+        currentSupply = sub(currentSupply, amount);
     }
 
     function lovequit()
     external {
-        uint256 holdings = ledger[msg.sender];
-        uint256 exitValue = calcBurnReward(holdings);
-        currentSupply = currentSupply.sub(holdings);
+        require(ledger[msg.sender] > 0, INVALID_ADDRESS);
+        uint holdings = ledger[msg.sender];
+        uint exitValue = calcBurnReward(holdings);
+        currentSupply = sub(currentSupply, holdings);
         contribute(exitValue, msg.sender);
         ledger[msg.sender] = 0;
     }
-
-    function contribute(uint256 amount, address sender)
+    
+    function contribute(uint amount, address sender)
     internal {
         beneficiary.transfer(amount);
-        contributions[sender] = contributions[sender].add(amount);
-        totalContributed = totalContributed.add(amount);
+        contributions[sender] = add(contributions[sender], amount);
+        totalContributed = add(totalContributed, amount);
     }
 
-    function integral(uint256 limitA, uint256 limitB, uint256 multiplier)
-    internal returns (uint256) {
-        uint256 raiseExp = exponent + 1;
-        uint256 _coefficient = coefficient.mul(multiplier).div(precision);
-        if (multiplier != 1) {
-            _coefficient = _coefficient.div(precision);
-        }
-        uint256 upper = precision.mul(limitB ** raiseExp).div(raiseExp).div(precision);
-        uint256 lower = precision.mul(limitA ** raiseExp).div(raiseExp).div(precision);
-        return _coefficient.mul(upper.sub(lower));
+    function integrate(uint limitA, uint limitB, uint multiplier)
+    internal returns (uint) {
+        uint raiseExp = exponent + 1;
+        uint _coefficient = wmul(coefficient, multiplier);
+        uint upper = wdiv((limitB ** raiseExp), raiseExp);
+        uint lower = wdiv((limitA ** raiseExp), raiseExp);
+        return wmul(_coefficient, (sub(upper, lower)));
+    }
+    
+    function calcMintPrice(uint amount)
+    internal returns (uint) {
+        return integrate(currentSupply, add(currentSupply, amount), precision);
     }
 
-    function calcMintPrice(uint256 amount)
-    public returns (uint256) {
-        return integral(currentSupply, currentSupply.add(amount), 1);
-    }
-
-    function calcBurnReward(uint256 amount)
-    public returns (uint256) {
-        return integral(currentSupply.sub(amount), currentSupply, reserveRatio);
+    function calcBurnReward(uint amount)
+    internal returns (uint) {
+        return integrate(sub(currentSupply, amount), currentSupply, reserveRatio);
     }
 }
